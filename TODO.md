9634ab8300a2e008f48ae74a7e7ef65f949ec0d0
Ivan Polanis <polanisivan@gmail.com>
2024-01-08 10:15:34 -0300

    adding types

--- TODO.md
+++ TODO.md
@@ -1,32 +1,9 @@
 # Tetris
-- Clase game (10 lineas por lvl).
-- - Board
-- - 
-- Tetromino.
-- UI
 
-
-Game modes
-A-Type: Endless, choice of starting level (0 through 19), clear 10 lines to advance to the next level.
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
+- Clase game (10 lineas por lvl). 
+- - Update de los bloque
+- -
+-  Tetromino
+- - Move, rotate y colission para tetromino y bloques (optiomizado).
+-- - Todo
+- UI ✅
--- tetris/game.py
+++ tetris/game.py
@@ -1,6 +1,5 @@
 import pygame
 import tkinter as tk
-import numpy as np
 from tetromino import Shape, Tetromino, Block
 import random
 from settings import COLS, ROWS, INITIAL_SPEED, WINDOW_WIDTH, WINDOW_HEIGHT, SCORE_DATA, MOVE_DIRECTION, WINDOW
@@ -26,9 +25,9 @@ class Game:
         self.surface = pygame.display.set_mode((WINDOW_WIDTH, WINDOW_HEIGHT))
         
         # Game logic
-        self.board: list = [[None for _ in range(COLS)] for _ in range(ROWS)]
+        self.board: list[list[Block | None]] = [[None] * COLS for _ in range(ROWS)]
         
-        self.cur_tetromino: Tetromino = Game.get_random_piece()
+        self.cur_tetromino: Tetromino = self.get_random_piece()
         self.speed: int = INITIAL_SPEED
 
         # UI and pygame
@@ -47,8 +46,8 @@ class Game:
             self.board[block.pos.x][block.pos.y] = block.primary_color
 
 
-    def check_lines(self) -> list:
-        checked_lines = []
+    def check_lines(self) -> list[int]:
+        checked_lines: list[int] = []
         for i in range(ROWS):
             valid_line = all(self.board[i][j] != 0 for j in range(COLS))
             if valid_line:
@@ -67,15 +66,15 @@ class Game:
                 self.board[i][j] = self.board[i+1][j]
 
 
-    def calculate_score(self,lines:list) -> None:
-        self.lines += lines.len()
-        self.score += SCORE_DATA(lines.len()) * (self.level + 1)
+    def calculate_score(self,lines:list[int]) -> None:
+        self.lines += len(lines)
+        self.score += SCORE_DATA[len(lines)] * (self.level + 1)
 
         if self.lines / 10 > self.level:
             self.level +=1
         
     
-    def get_random_piece() -> Tetromino:
+    def get_random_piece(self) -> Tetromino:
         type = random.choice(list(Shape))
         piece = Tetromino(type)
         return piece
@@ -85,16 +84,16 @@ class Game:
             if((event.type == pygame.QUIT) or (event.type == pygame.K_ESCAPE)):
                 pygame.quit()
                 sys.exit()
-            elif(event.type == pygame.K_LEFT):
-                self.cur_tetromino.move(MOVE_DIRECTION["LEFT"])
-            elif(event.type == pygame.K_RIGHT):
-                self.cur_tetromino.move(MOVE_DIRECTION["RIGHT"])
-            elif(event.type == pygame.K_DOWN):
-                self.cur_tetromino.move(MOVE_DIRECTION["DOWN"])
-            elif(event.type == pygame.K_z or event.type == pygame.K_Z):
-                self.cur_tetromino.rotate()
-            elif(event.type == pygame.K_x or event.type == pygame.K_X):
-                self.cur_tetromino.rotate()
+            # elif(event.type == pygame.K_LEFT):
+            #     self.cur_tetromino.move(MOVE_DIRECTION["LEFT"])
+            # elif(event.type == pygame.K_RIGHT):
+            #     self.cur_tetromino.move(MOVE_DIRECTION["RIGHT"])
+            # elif(event.type == pygame.K_DOWN):
+            #     self.cur_tetromino.move(MOVE_DIRECTION["DOWN"])
+            # elif(event.type == pygame.K_z or event.type == pygame.K_Z):
+            #     self.cur_tetromino.rotate()
+            # elif(event.type == pygame.K_x or event.type == pygame.K_X):
+            #     self.cur_tetromino.rotate()
 
 
     def run(self):
@@ -108,7 +107,7 @@ class Game:
             self.board_ui.run(self.board)
             self.score_ui.run()
             self.preview_ui.run()
-            self.tetromino_ui(self.cur_tetromino)
+            # self.tetromino_ui(self.cur_tetromino)
     
             #update
             self.update()
@@ -119,7 +118,7 @@ class Game:
 
     def update(self):
         pygame.display.update()
-        self.cur_tetromino.move(MOVE_DIRECTION["DOWN"])
+        # self.cur_tetromino.move(MOVE_DIRECTION["DOWN"])
         pygame.time.delay(1000)
 
 
